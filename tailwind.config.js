e9b89cb96e30dc402e575137232115cd6b360974
Valentina <valentina.donigaglia.design@gmail.com>
2025-09-10 12:12:35 +0200

    configuration colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        coffee: {
+          500: "#331B07", // cta, section darkmode
+        },
+        beige: {
+          50: "#E8DCCF", //cta text
+          200: "#DCC8B2", // testi chiari, cta light mode
+          footer: "#F5E9DD", // footer light mode
+        },
+      },
+      fontFamily: {
+        sans: ["'Albert Sans'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
-
